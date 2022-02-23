16abc6a1cf176cb6e4dbf55afbbac5a0be5bf6a9
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-02-23 01:40:45 -0300

    chore: format add-integration script

--- src/add-integration.mjs
+++ src/add-integration.mjs
@@ -2,21 +2,20 @@ import { v4 } from 'uuid';
 import updateIntegrations from './updateIntegrations.mjs';
 
 const run = async () => {
-  await updateIntegrations('feat: add new integration', (integrations) => {
+  await updateIntegrations('feat: add a new integration', (integrations) => {
     const newIntegration = {
       id: v4(),
       name: process.env.INTEGRATION_NAME,
       gitRepositoryURL: process.env.INTEGRATION_GIT_REPOSITORY_URL,
       createdAt: new Date().toISOString(),
-      updatedAt: new Date().toISOString()
+      updatedAt: new Date().toISOString(),
     };
 
     return [...integrations, newIntegration];
   });
 };
 
-run()
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+run().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
